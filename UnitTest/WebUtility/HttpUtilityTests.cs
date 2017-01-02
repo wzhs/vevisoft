782fa895006c2b2421d888b624cc85a039de3f92
admin <wwei466@gmail.com>
2017-01-02 22:08:43 +0800

    添加测试项目

---
+++ UnitTest/WebUtility/HttpUtilityTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vevisoft.WebUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vevisoft.WebUtility.Tests
+{
+    [TestClass()]
+    public class HttpUtilityTests
+    {
+        [TestMethod()]
+        public void GetHtmlStringFromUrlByGetTest()
+        {
+            HttpParam hp = new HttpParam("http://www.baidu.com");
+            string str = HttpUtility.GetHtmlStringFromUrlByGet(hp);            
+            Assert.IsTrue(str.Length > 0);
+            //Assert.Fail();
+        }
+    }
+}
---
+++ vevisoftTests/WebUtility/HttpUtilityTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vevisoft.WebUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vevisoft.WebUtility.Tests
+{
+    [TestClass()]
+    public class HttpUtilityTests
+    {
+        [TestMethod()]
+        public void GetHtmlStringFromUrlByGetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
